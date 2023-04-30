3bfbe03cb5ae997a446a6a455d6c5dd74066194d
Emir Orsini <deranged.ve@gmail.com>
2023-04-30 10:07:43 -0400

    The ULTIMATE FAFO!!!!

--- public/script.js
+++ public/script.js
@@ -50,6 +50,7 @@ peer.on("open", (id) => {
     sessionStorage.setItem("userId", key)
     socket.emit("join-room", ROOM_ID, id);
 });
+
 const addVideoStream = (template, stream) => {
     const video = template.querySelector('video');
     video.muted = true;
@@ -60,32 +61,82 @@ const addVideoStream = (template, stream) => {
     });
 };
 
+const videoButton = document.getElementById('toggle-video-button');
+videoButton.classList.add('active');
+
+const toggleVideo = (element) => {
+  const placeholder = document.querySelector('.placeholder');
+
+  if (element.classList.contains('active')) {
+    element.classList.remove('active');
+    myVideo?.pause();
+    myVideo?.style.setProperty('display', 'none');
+    placeholder.style.display = 'block';
+  } else {
+    element.classList.add('active');
+    myVideo?.play();
+    myVideo?.style.setProperty('display', 'block');
+    placeholder.style.display = 'none';
+  }
+};
+
 const toggleMute = (element) => {
+  element.classList.toggle('active');
+  myVideo.muted = !myVideo.muted;
+};
+
+const toggleReset = (element) => {
+    const box = document.querySelector('.box');
+    box.value = 40;
+    changeInputColor(box, box.value);
     if (element.classList.contains('active')) {
-        element.classList.remove('active')
+      element.classList.remove('active');
+    } else {
+      element.classList.add('active');
     }
-    else {
-        element.classList.add('active')
-    }
+  };
+  
 
-    myVideo.mute = !myVideo.mute
-}
-
-const box = document.querySelector(".box");
-
-document.addEventListener("keydown", (event) => {
-  // Check if the key pressed is up or down arrow
-  if (event.keyCode === 38 || event.keyCode === 40) {
-    // Prevent the default action of the key press
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
     event.preventDefault();
 
-    // Check if the target of the event is the box
-    if (event.target === box) {
-      // Step the box up or down
-      box.stepUp(event.keyCode === 38 ? 1 : -1);
-    } else {
-      // Step the default box up or down
-      document.querySelector(".box").stepUp(event.keyCode === 38 ? 1 : -1);
-    }
+    const box = document.querySelector('.box');
+    if (!box) return;
+
+    box.value = parseInt(box.value) + (event.key === 'ArrowUp' ? 1 : -1);
+
+    if (myVideo && !myVideo.paused) {
+        if (box.value <= 0 && !document.querySelector('.overlay')) {
+          toggleVideo(videoButton);
+          const overlay = document.createElement('div');
+          overlay.classList.add('overlay');
+          const message = document.createElement('p');
+          message.innerText = 'YOU DIED!';
+          overlay.appendChild(message);
+          document.querySelector('.cell').appendChild(overlay);
+        }
+        else if (myVideo.paused && document.querySelector('.overlay')) {
+          document.querySelector('.overlay').remove();
+          toggleVideo(videoButton);
+        }
+      }
+      
+
+    changeInputColor(box, box.value);
   }
 });
+
+function changeInputColor(input, value) {
+    input.classList.remove("box-red", "box-yellow", "box-white", "box-green");
+    if (value <= 10) {
+        input.classList.add("box-red");
+    } else if (value >= 11 && value <= 20) {
+        input.classList.add("box-yellow");
+    } else if (value >= 40) {
+        input.classList.add("box-green");
+    } else {
+        input.classList.add("box-white");
+    }
+}
+
