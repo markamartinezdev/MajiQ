e2021f204f649fda54320e9acd21c2a5786b9d3c
Emir Orsini <deranged.ve@gmail.com>
2023-05-02 06:42:28 +0000

    Created Self-Pushing Script (I think)

---
+++ docker-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Combine the commands using semicolons
+docker-compose down --volumes ; \
+docker rmi mtgtable ; \
+docker build -t mtgtable . --no-cache ; \
+docker-compose up -d
+
+# Finished
+echo "Docker setup completed."
