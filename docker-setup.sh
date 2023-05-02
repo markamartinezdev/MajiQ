0c8b15faf4af80631ec145457be202453c2f05f4
Emir Orsini <deranged.ve@gmail.com>
2023-05-02 21:38:20 +0000

    Corrected docker setup script (Redeployment)

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Combine the commands using semicolons
-docker-compose down --volumes ; \
-docker rmi mtgtable ; \
-docker build -t mtgtable . --no-cache ; \
-'docker-compose up -d'
+# Step 1: Bring down the docker containers and remove volumes
+docker compose down --volumes
+
+# Step 2: Remove the image with the specified name
+docker rmi mtgtable
+
+# Step 3: Build the image again, using the specified tag and no cache
+docker build -t mtgtable . --no-cache
+
+# Step 4: Start the containers in detached mode
+docker compose up -d
 
 # Finished
 echo "Docker setup completed."
-git status
