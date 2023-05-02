c913cccabaa932696dac87319932b3b23c394cf4
Emir Orsini <deranged.ve@gmail.com>
2023-05-02 21:33:06 +0000

    update dockersetup

--- docker-setup.sh
+++ docker-setup.sh
@@ -4,8 +4,8 @@
 docker-compose down --volumes ; \
 docker rmi mtgtable ; \
 docker build -t mtgtable . --no-cache ; \
-docker-compose up -d
+'docker-compose up -d'
 
 # Finished
 echo "Docker setup completed."
-git st
+git status
